9dcdb165da4c196fea9bb54d89b8f7b7975f9721
dennis adrian <adriangbetancur@gmail.com>
2021-01-26 16:21:11 -0400

    Main class configured to just open a virtual machine with a given name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,37 +6,26 @@ import org.virtualbox_6_1.IMediumFormat;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        /* System.out.println("got event: " + ev);
-        VBoxEventType type = ev.getType();
-        System.out.println("type = " + type);  */
-
         VBoxManager vBoxMgr = new VBoxManager();
-        String result = vBoxMgr.getVBoxVersion();
+        Scanner input = new Scanner(System.in);
         try {
+            System.out.println("Welcome!");
+            System.out.println("What is the name of the machine to be launched?");
+            String machineName = input.nextLine();
 
-            //vBoxMgr.createVM("ubuntu linux");
-           //vBoxMgr.openLastVM();
-            //vBoxMgr.listAllVMs();
-            //vBoxMgr.getVM("Fedora 3");
-            //vBoxMgr.installationProcess();
-            //vBoxMgr.powerOffMachine("My Ubuntu Machine");
-            //vBoxMgr.removeVM("my new machine");
-            //vBoxMgr.launchVM("Fedora 33");
-            vBoxMgr.createVM("Ubuntu 20.04.1 Server");
-
+            vBoxMgr.launchVM(machineName);
             System.out.println("done, press Enter...");
             int ch = System.in.read();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
         vBoxMgr.cleanup();
     }
 }
